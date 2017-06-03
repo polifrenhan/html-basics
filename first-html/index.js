876e031bab4eb81d0c0f1b12e4c6be7b2281770d
Poli <polianasantos@gec.inatel.br>
2017-06-03 14:30:21 -0300

    js

--- first-html/index.js
+++ first-html/index.js
@@ -1,1 +1,14 @@
 console.log('Hi there!');
+
+for(let i=1;i<10;i++){
+    console.log('Hi number '+i);
+}
+
+//function sleep(milliSeconds){
+//    let startTime = Date.now()
+//    while(Date.now() <startTime + milliSeconds);
+//}
+
+//sleep(2000);
+
+//console.log('Hi there after sleep')
